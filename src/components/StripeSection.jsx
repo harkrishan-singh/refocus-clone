3c19e452f8249cb7f9acf49606eca716e0c1d20f
Harkrishan Singh <git>
2025-01-23 03:09:20 +0530

    Updated stripe image export

--- src/components/StripeSection.jsx
+++ src/components/StripeSection.jsx
@@ -1,22 +1,8 @@
 import { useEffect, useState } from "react";
 import Stripe from "./Stripe";
+import { StripeData } from "../helpers/StripeData";
 
 const StripeSection = () => {
-  const stripeData = [
-    {
-      url: "src/assets/stripe-img-1.svg",
-      number: 48,
-    },
-    {
-      url: "src/assets/stripe-img-2.svg",
-      number: 2,
-    },
-    {
-      url: "src/assets/stripe-img-3.svg",
-      number: 12,
-    },
-  ];
-
   const [innerWidth, setInnerWIdth] = useState(window.innerWidth);
 
   useEffect(() => {
@@ -30,11 +16,11 @@ const StripeSection = () => {
 
   return (
     <div className=" w-full py-10 flex justify-between items-center ">
-      {stripeData.map((data, index) => (
+      {StripeData.map((data, index) => (
         <Stripe key={index} data={data} />
       ))}
       {innerWidth > 640
-        ? stripeData.map((data, index) => <Stripe key={index} data={data} />)
+        ? StripeData.map((data, index) => <Stripe key={index} data={data} />)
         : null}
     </div>
   );
---
+++ src/helpers/StripeData.js
@@ -0,0 +1,18 @@
+import stripe1 from "../assets/stripe-img-1.svg";
+import stripe2 from "../assets/stripe-img-2.svg";
+import stripe3 from "../assets/stripe-img-3.svg";
+
+export const StripeData = [
+  {
+    url: stripe1,
+    number: 48,
+  },
+  {
+    url: stripe2,
+    number: 2,
+  },
+  {
+    url: stripe3,
+    number: 12,
+  },
+];
